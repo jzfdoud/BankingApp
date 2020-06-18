2f23c4d41621b0460c25251d79c029091da1891f
Jada <jada.fagan@gmail.com>
2020-06-18 16:28:58 -0400

    Add project files.

---
+++ BankingApp/Account.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingApp
+{
+    class Account
+    {
+
+        public int AccountNumber { get; set; }
+        public double Balance { get; set; } = 0;
+        public string Description { get; set; }
+
+
+        public void Deposit(double Amount)
+        {
+            if (Amount <= 0)
+            {
+                Console.WriteLine("Invalid Deposit Amount, Amount must be positive");
+                return;
+            }
+            Balance += Amount;
+        }
+
+        public void Withdrawl(double Amount)
+        {
+            if (Amount > Balance || Amount <= 0)
+            {
+                Console.WriteLine("Insufficient Funds");
+                return;
+            }
+            Balance -= Amount;
+        }
+
+        public double GetBalance()
+        {
+            return Balance;
+        }
+
+
+        public Account() {
+            this.AccountNumber = 0;
+            this.Description = "New Account";
+
+
+        }
+    }
+}
---
+++ BankingApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var acct1 = new Account();
+            acct1.Deposit(100000);
+            acct1.Withdrawl(60000);
+            acct1.Withdrawl(50000);
+            acct1.Deposit( -20000);
+            acct1.Withdrawl(-10000);
+        }
+    }
+}
